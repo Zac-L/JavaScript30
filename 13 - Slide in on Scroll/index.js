1e477e2ff7ac9a13a28d6a6198d1e5894262742f
Zac-L <Zachlichens@gmail.com>
2018-04-14 19:51:47 -0700

    updated debounce function

--- 13 - Slide in on Scroll/index.js
+++ 13 - Slide in on Scroll/index.js
@@ -1,24 +1,37 @@
 
 
-// debounce function taken from https://davidwalsh.name/javascript-debounce-function
-function debounce(func, wait = 20, immediate = true) {
-  var timeout;
-  return function() {
-    var context = this, args = arguments;
-    var later = function() {
-      timeout = null;
-      if (!immediate) func.apply(context, args);
-    };
-    var callNow = immediate && !timeout;
+// debounce function taken from: https://davidwalsh.name/javascript-debounce-function
+// function debounce(func, wait = 20, immediate = true) {
+//   var timeout;
+//   return function() {
+//     var context = this, args = arguments;
+//     var later = function() {
+//       timeout = null;
+//       if (!immediate) func.apply(context, args);
+//     };
+//     var callNow = immediate && !timeout;
+//     clearTimeout(timeout);
+//     timeout = setTimeout(later, wait);
+//     if (callNow) func.apply(context, args);
+//   };
+// }
+
+// ES6 version taken from: https://medium.com/@TCAS3/debounce-deep-dive-javascript-es6-e6f8d983b7a1
+const debounce = (fn, time) => {
+  let timeout;
+
+  return function(...args) {
+    const functionCall = () => fn.apply(this, args);
+    
     clearTimeout(timeout);
-    timeout = setTimeout(later, wait);
-    if (callNow) func.apply(context, args);
-  };
+    timeout = setTimeout(functionCall, time);
+  }
 }
 
 const sliderImages = document.querySelectorAll('.slide-in');
 
 const checkSlide = event => {
+  console.log(event);
   sliderImages.forEach(sliderImage => {
     const slideInAt = (window.scrollY + window.innerHeight) - sliderImage.height / 2;
     const imageBottom = sliderImage.offsetTop + sliderImage.height;
@@ -33,4 +46,4 @@ const checkSlide = event => {
   });
 }
 
-window.addEventListener('scroll', debounce(checkSlide));
+window.addEventListener('scroll', debounce(checkSlide, 20));
